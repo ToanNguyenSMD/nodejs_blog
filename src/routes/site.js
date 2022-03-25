f3b1bd2e887358a3134cdd4132f29c82798086ea
ToanNguyen <toan.nguyen@smartdev.com>
2022-03-25 12:02:17 +0700

    test

--- src/routes/site.js
+++ src/routes/site.js
@@ -4,6 +4,6 @@ const router = express.Router()
 const siteController = require('../app/controllers/SiteController')
 
 router.use('/search', siteController.search)
-router.use('/', siteController.index) // cái này luôn nằm dưới cùng, vì route nó đọc từ trên xuống
+router.use('/', siteController.index) // cái này luôn nằm dưới cùng, vì route nó đọc từ trên xuống.
 
 module.exports = router
