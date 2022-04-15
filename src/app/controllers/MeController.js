522d354dac166ab0362a980bf8ef317ab76b0358
ToanNguyen <toan.nguyen@smartdev.com>
2022-04-15 17:18:31 +0700

    count deleted

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -4,14 +4,79 @@ class MeController {
 
   //[GET] /me/storedCourses
   storedCourses(req, res, next) {
-    Course.find({})
-      .then(courses =>{
+
+    
+
+
+
+    Promise.all([Course.find(), Course.countDocumentsDeleted()])
+      .then(([courses, deletedCount]) => {
+        // let data = [
+        //   {
+        //     start: '12',
+        //     end: '20'
+        //   },
+        //   {
+        //     start: '13',
+        //     end: '22'
+        //   },
+        //   {
+        //     start: '12',
+        //     end: '20'
+        //   },
+        //   {
+        //     start: '14',
+        //     end: '21'
+        //   },
+        //   {
+        //     start: '13',
+        //     end: '11'
+        //   },
+        // ]
+        // var countList = data.reduce(function(p, c){
+          
+        //   p[c.start] = (p[c.start] || 0) + 1;
+        //   return p;
+        // }, {});
+        
+        // var result = data.filter(function(obj){
+         
+        //   return countList[obj.start] == 2;
+        // });
+        // let xx = []
+        // data.forEach(element => {
+        //   if(countList[element.start] == 2) {
+        //     xx.push(element.start)
+        //   }
+        // })
+        // console.log('xx', xx)
+        // console.log('countList1', countList)
+        // console.log('result1', result)
+        
         res.render('me/stored-courses', {
-          courses: multipleMongooseToObject(courses)
+          courses: multipleMongooseToObject(courses),
+          deletedCount
         }
         );
       })
       .catch(next)
+
+
+    // Course.countDocumentsDeleted()
+    //   .then(deletedCount=>{
+    //     console.log('deletedCount', deletedCount)
+    //   })
+    //   .catch(()=>{
+        
+    //   })
+    // Course.find()
+    //   .then(courses =>{
+    //     res.render('me/stored-courses', {
+    //       courses: multipleMongooseToObject(courses)
+    //     }
+    //     );
+    //   })
+    //   .catch(next)
   }
 
   //[GET] /me/trash/courses
